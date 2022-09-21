50974460c3dbdc2f291412aa3af4076b5e16cf83
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-09-21 08:02:57 +0700

    +variable in go

---
+++ variable3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var name string
+
+	name := "Robert Dodi"
+	fmt.Println(name)
+
+	name = "Zaed Abdullah"
+	fmt.Println(name)
+}
---
+++ variable4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var (
+		firstName = "Zaed"
+		lastName = "Abdullah"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
